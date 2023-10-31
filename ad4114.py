6554ef8328aaa1fce9fda621cab105a20ab752dc
Unweptpit <71827483+KCHBloem@users.noreply.github.com>
2023-10-31 17:17:11 +0100

    Updated Library

--- ad4114.py
+++ ad4114.py
@@ -1,6 +1,11 @@
 import spidev
 class AD4114:
  ID_REGISTER = 0x07
+ COMM_REGISTER = 0x00
+ CHANNEL_0_REGISTER = 0x10
+ DATA_REGISTER = 0x04
+ ADC_MODE_REGISTER = 0x01
+ SETUP_REGISTER_0 = 0x20
 
 
  def __init__(self, mode, speed):
@@ -15,9 +20,59 @@ class AD4114:
    data.append(0x00)
   
   received = self.spi.xfer2(data)
-  return (received[1] << 8) | received[2]
-
+  if (bytes == 2):
+   return (received[1] << 8) | received[2]
+  if (bytes == 3):
+   return received[1] << 16 | received[2] << 8 | received[3]
+   
  def read_ID_Register(self):
   return self.read_Register(self.ID_REGISTER, 2) 
  
+ #Write
+ 
+ def write(self, channel, data):
+  message = [self.COMM_REGISTER | channel]
+  data1 = (data>>8) & (0xFF)
+  data2 = data & 0xFF
+  message.append(data1)
+  message.append(data2) 
+  	
+  self.spi.xfer2(message)
   
+ def disable_channel(self, channel):
+   channel = self.CHANNEL_0_REGISTER + channel
+   self.write(channel, (self.read_Register(channel, 2) & 0x7FFF))	
+   	
+ def enable_channel(self, channel):
+   channel = self.CHANNEL_0_REGISTER + channel
+   self.write(channel, (self.read_Register(channel, 2) | 0x8000))
+   	
+ def read_channel(self, channel):
+  return self.read_Register((self.CHANNEL_0_REGISTER + channel), 2)
+ 
+ def set_channel_input(self, channel, data):
+   channel = self.CHANNEL_0_REGISTER + channel
+   self.write(channel, (self.read_Register(channel, 2) & 0xFC00))
+   self.write(channel, (self.read_Register(channel, 2) | data))
+ 
+ def read_data(self):
+   return self.read_Register(self.DATA_REGISTER, 3)
+   
+ def read_adc_mode(self):
+   return self.read_Register(self.ADC_MODE_REGISTER, 2)
+
+ def set_adc_mode(self, Ref_EN, Sing_Cyc, Delay, Mode, Clock):
+   data = (Ref_EN << 15) | (Sing_Cyc << 13) | (Delay << 8) | (Mode << 4) | (Clock << 2)
+   self.write(self.ADC_MODE_REGISTER, data)
+ 
+ def read_setup_configuration(self, number):
+   reg = self.SETUP_REGISTER_0 + number;
+   return self.read_Register(reg, 2)
+
+
+ def set_setup_configuration(self, number, BI_UNIPOLAR, REFBUF_P, REFBUF_N, INBUF, REF_SEL):
+  reg = self.SETUP_REGISTER_0 + number;
+  data = (BI_UNIPOLAR << 12) | (REFBUF_P << 11) | (REFBUF_N << 10) | (INBUF << 8) | (REF_SEL << 4)
+  self.write(reg, data)
+ 
+ 
--- main.py
+++ main.py
@@ -5,15 +5,35 @@ import time
 from ad4114 import AD4114 
 
 
+vref = 2.5
+n = 24
+
 wpi.wiringPiSetup()
 wpi.pinMode(0, 1)
 
 adc = AD4114(0b11, 25000)
 
 print(hex(adc.read_ID_Register()))
+adc.set_adc_mode(1, 1, 7, 0, 0)
+adc.set_setup_configuration(0, 1, 0, 0, 0, 2)
+
+for x in range(15):
+ adc.disable_channel(x)
+ 
+adc.enable_channel(15)
+adc.set_channel_input(15, 16)
+print(adc.read_channel(15))
+
+
+
+
+
 
 while True:
-    
+    code = adc.read_data()
+
+    vin = (((code / (2**(n-1))) - 1) * vref * 10)
+    print(vin)
     wpi.digitalWrite(0, 1)
     time.sleep(1)
     wpi.digitalWrite(0, 0)
